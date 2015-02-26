205e08b14a41cd1ed0e911c0b762e7f842d8b5a6
Paco Gomez <contact@pacogomez.com>
2015-02-25 18:08:34 -0600

    wip

---
+++ conf/configure.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+sudo -i
+
+PRJ=/home/ubuntu/score-service
+
+service nginx stop
+rm /etc/nginx/sites-enabled
+ln -s $PRJ/conf/nginx/vca_io /etc/nginx/sites-enabled
+service nginx start
+
---
+++ conf/install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+sudo -i
+
+apt-get update
+apt-get install -y nginx git
+
+apt-get install -y build-essential libssl-dev libffi-dev libxml2-dev libxslt-dev python-dev python-pip 
