cc85e159054e89a3d8b826c2f580cfc75c506f46
Paco Gomez <pgomez@vmware.com>
2015-02-26 00:41:00 +0000

    wip

--- conf/configure.sh
+++ conf/configure.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-sudo -i
+#as root
+#sudo -i
 
 PRJ=/home/ubuntu/score-service
 
 service nginx stop
-rm /etc/nginx/sites-enabled
+rm /etc/nginx/sites-enabled/*
 ln -s $PRJ/conf/nginx/vca_io /etc/nginx/sites-enabled
 service nginx start
 
