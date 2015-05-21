7b5f4951c4c9ea786e5d5a08ea771c6cccf97b84
Paco Gomez <pgomez@vmware.com>
2015-05-21 18:21:33 +0000

    fixed env

--- conf/run.sh
+++ conf/run.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-if [ -z "PRJ" ]; then
+if [ -z "$PRJ" ]; then
     export PRJ=~/score-service
 fi
 
 cd $PRJ
 source $PRJ/venv/bin/activate
 cd $PRJ/score-api-server
-if [ -z "CFY_HOST" ]; then
+if [ -z "$CFY_HOST" ]; then
     export CFY_HOST=192.168.240.2
 fi    
-if [ -z "CFY_PORT" ]; then
+if [ -z "$CFY_PORT" ]; then
     export CFY_PORT=80
 fi
 $PRJ/venv/bin/gunicorn -w 4 -b 127.0.0.1:8001 score_api_server.app:app
