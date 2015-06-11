03a6d32e8becb2bda7718517a397dfdca5461110
Denys Makogon <Denys@gigaspaces.com>
2015-06-11 13:43:25 +0300

    Fix run script

--- etc/scripts/run.sh
+++ etc/scripts/run.sh
@@ -8,23 +8,23 @@ cd $PRJ
 source $PRJ/.venv/bin/activate
 cd $PRJ/score-api-server
 
-if [ -z "$SCORE_HOST" ]; them
+if [ -z "$SCORE_HOST" ]; then
     export SCORE_HOST=0.0.0.0
 fi
 
-if [ -z "$SCORE_PORT" ]; them
+if [ -z "$SCORE_PORT" ]; then
     export SCORE_PORT=5000
 fi
 
-if [ -z "$SCORE_WORKERS" ]; them
+if [ -z "$SCORE_WORKERS" ]; then
     export SCORE_WORKERS=4
 fi
 
-if [ -z "$CFY_MANAGER_HOST" ]; them
+if [ -z "$CFY_MANAGER_HOST" ]; then
     export CFY_MANAGER_HOST=127.0.0.1
 fi
 
-if [ -z "$CFY_MANAGER_PORT" ]; them
+if [ -z "$CFY_MANAGER_PORT" ]; then
     export CFY_MANAGER_PORT=80
 fi
 
