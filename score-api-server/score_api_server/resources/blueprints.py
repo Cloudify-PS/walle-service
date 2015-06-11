ea6571491460df8f518d2c05f9273e4df3cd8e82
Kostya <konstantin@gigaspaces.com>
2015-06-11 11:50:53 +0400

    ScoreSCOR-5 Change the Score Rest server to allow ids without Org-ids

--- score-api-server/score_api_server/resources/blueprints.py
+++ score-api-server/score_api_server/resources/blueprints.py
@@ -4,9 +4,11 @@ import os
 import tempfile
 import tarfile
 import shutil
+import os.path
 
 from flask.ext import restful
 from flask import request, g
+from score_api_server.resources import add_org_prefix, remove_org_prefix
 
 
 # Chunk is handled by gunicorn
@@ -22,24 +24,22 @@ def decode(input_stream, buffer_size=8192):
 class Blueprints(restful.Resource):
 
     def get(self, blueprint_id=None):
+
         if blueprint_id is not None:
-            if not blueprint_id.startswith(g.org_id + '_'):
-                return
-            return g.cc.blueprints.get(blueprint_id)
+            return g.cc.blueprints.get(add_org_prefix(blueprint_id))
         else:
             blueprints = g.cc.blueprints.list()
             result = []
             for blueprint in blueprints:
                 if blueprint.id.startswith(g.org_id + '_'):
-                    result.append(blueprint)
+                    result.append(remove_org_prefix(blueprint))
             return result
 
     def put(self, blueprint_id):
         tempdir = tempfile.mkdtemp()
         try:
-            archive_file_name = tempdir + '/' + blueprint_id + '.tar.gz'
+            archive_file_name = os.path.join(tempdir, add_org_prefix(blueprint_id) + '.tar.gz')
             self._save_file_locally(archive_file_name)
-
             tfile = tarfile.open(archive_file_name, 'r:gz')
             tfile.extractall(tempdir)
             files = os.listdir(tempdir)
@@ -48,23 +48,21 @@ class Blueprints(restful.Resource):
                 if not file.endswith('.tar.gz'):
                     directory = file
                     break
-
             blueprint = g.cc.blueprints.upload(
-                tempdir + '/' + directory +
-                '/blueprint.yaml', blueprint_id)
+                os.path.join(tempdir, 'blueprint.yaml'),
+                add_org_prefix(blueprint_id))
             return blueprint, 201
         finally:
             shutil.rmtree(tempdir)
 
     def delete(self, blueprint_id):
-        blueprint = g.cc.blueprints.delete(blueprint_id)
+        blueprint = g.cc.blueprints.delete(add_org_prefix(blueprint_id))
         return blueprint
 
     @staticmethod
     def _save_file_locally(archive_file_name):
-
         if 'Transfer-Encoding' in request.headers:
-            with open(archive_file_name, 'w') as f:
+            with open(archive_file_name, 'wb') as f:
                 for buffered_chunked in decode(request.input_stream):
                     f.write(buffered_chunked)
         else:
@@ -73,5 +71,5 @@ class Blueprints(restful.Resource):
                     'Missing application archive in request body or '
                     '"blueprint_archive_url" in query parameters')
             uploaded_file_data = request.data
-            with open(archive_file_name, 'w') as f:
+            with open(archive_file_name, 'wb') as f:
                 f.write(uploaded_file_data)
