b4b3b0ba3771ef037c63cad9ee0d2694eb2e64b5
Denis Pauk <pauk.denis@gmail.com>
2015-06-12 11:44:02 +0300

    SCOR-45: add initial postgresql blueprint

---
+++ blueprints/scripts/install_postgresql.py
@@ -0,0 +1,14 @@
+import fabric
+from cloudify import ctx
+
+def _run(command):
+    ctx.logger.info(command)
+    out = fabric.api.run(command)
+    ctx.logger.info(out)
+
+def install(config):
+    ctx.logger.info("Config: " + str(config))
+    _run("""
+sudo apt-get update 2>&1
+sudo apt-get install -y postgresql postgresql-contrib 2>&1
+    """)
