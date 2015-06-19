e8365ccc9e9fa9aed63fc6ad84df8985de66f28d
Denis Pauk <pauk.denis@gmail.com>
2015-06-19 13:05:55 +0300

    add default testing keys for nginx

---
+++ key-gen/gen_key.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+echo "please use 'score' as pass phrase"
+openssl genrsa -des3 -out server.key 2048
+# Signing request
+openssl req -new -key server.key -out server.csr -batch
+# Remove the Passphrase
+cp server.key server.key.org
+openssl rsa -in server.key.org -out server.key
+# Sign your SSL Certificate
+openssl x509 -req -days 365 -in server.csr -signkey server.key -out server.crt
+mkdir -p ../etc/keys
+mv server.crt server.key ../etc/keys
