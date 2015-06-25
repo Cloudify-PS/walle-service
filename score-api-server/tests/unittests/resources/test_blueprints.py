9481ba4b203d1425d698fa96fe300b78035f3bfd
Mykhailo Durnosvystov <Mikhail@gigaspaces.com>
2015-06-25 10:18:35 +0300

    Added unit tests for blueprints

--- score-api-server/tests/unittests/resources/test_blueprints.py
+++ score-api-server/tests/unittests/resources/test_blueprints.py
@@ -14,7 +14,9 @@ class TestBase(unittest.TestCase):
 
     def setUp(self):
         super(TestBase, self).setUp()
+        self.bp_id = 'foo'
         self.app = flask.Flask(__name__)
+        self.bluprient = blueprints.Blueprints()
         self.addCleanup(super(TestBase, self).tearDown)
 
     @mock.patch.object(os, 'listdir')
@@ -22,20 +24,49 @@ class TestBase(unittest.TestCase):
     @mock.patch.object(tarfile.TarFile, 'extractall')
     @mock.patch.object(blueprints.Blueprints, '_save_file_locally')
     @mock.patch.object(tempfile, 'mkdtemp')
-    def test_remove_org_prefix(self, mock_mkdtemp, mock_save_file,
-                               mock_extractall, mock_open, mock_listdir):
+    def test_put(self, mock_mkdtemp, mock_save_file,
+                 mock_extractall, mock_open, mock_listdir):
         mock_mkdtemp.return_value = '/tmp'
-        bp_id = 'foo'
-        mock_listdir.return_value = [bp_id]
-        bluprient = blueprints.Blueprints()
+
+        mock_listdir.return_value = [self.bp_id]
         with self.app.app_context():
             flask.g.org_id = "some_id"
             flask.g.cc = mock.MagicMock()
-            fake_id = flask.g.org_id + '_' + bp_id
+            fake_id = flask.g.org_id + '_' + self.bp_id
             fake_bp = fake_objects.FakeBlueprint(fake_id, fake_id, fake_id)
-            flask.g.cc.blueprints.upload = mock.MagicMock(
-                return_value=fake_bp)
+            flask.g.cc.blueprints.upload = mock.MagicMock(return_value=fake_bp)
             with self.app.test_request_context('/?application_file_name=bar'):
-                bp, status = bluprient.put(bp_id)
+                bp, status = self.bluprient.put(self.bp_id)
 
-        self.assertEqual(bp_id, bp.id)
+        self.assertEqual(self.bp_id, bp.id)
+
+    def test_get_with_blueprint_id(self):
+        with self.app.app_context():
+            flask.g.org_id = "some_id"
+            flask.g.cc = mock.MagicMock()
+            fake_id = flask.g.org_id + '_' + self.bp_id
+            fake_bp = fake_objects.FakeBlueprint(fake_id, fake_id, fake_id)
+            flask.g.cc.blueprints.get = mock.MagicMock(return_value=fake_bp)
+            bp = self.bluprient.get(blueprint_id=self.bp_id)
+        self.assertEqual(fake_id, bp.id)
+
+    def test_get_all_blueprints(self):
+        with self.app.app_context():
+            flask.g.org_id = "some_id"
+            flask.g.cc = mock.MagicMock()
+            fake_id = flask.g.org_id + '_' + self.bp_id
+            fake_bp = fake_objects.FakeBlueprint(fake_id, fake_id, fake_id)
+            flask.g.cc.blueprints.list = mock.MagicMock(return_value=[fake_bp])
+            bps = self.bluprient.get()
+        self.assertEqual(self.bp_id, bps[0].id)
+
+    def test_delete(self):
+        with self.app.app_context():
+            flask.g.org_id = "some_id"
+            flask.g.cc = mock.MagicMock()
+            fake_id = flask.g.org_id + '_' + self.bp_id
+            fake_bp = fake_objects.FakeBlueprint(fake_id, fake_id, fake_id)
+            flask.g.cc.blueprints.delete = mock.MagicMock(
+                return_value=fake_bp)
+            bp = self.bluprient.delete(blueprint_id=self.bp_id)
+        self.assertEqual(fake_id, bp.id)
