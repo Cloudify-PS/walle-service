3ad643b4cda8a602150827f41929fd121589e0a9
Denis Pauk <pauk.denis@gmail.com>
2015-06-26 13:06:33 +0300

    SCOR-52: fix typo in mongo install

--- blueprints/scripts/install_mongo.py
+++ blueprints/scripts/install_mongo.py
@@ -4,11 +4,10 @@ import fabric
 from cloudify import ctx
 
 
-def _run(commands):
-    ctx.logger.info(commands)
-    for command in commands:
-        out = fabric.api.run(command)
-        ctx.logger.info(out)
+def _run(command):
+    ctx.logger.info(command)
+    out = fabric.api.run(command)
+    ctx.logger.info(out)
 
 
 def install(config):
@@ -19,12 +18,12 @@ def install(config):
         'echo "deb http://repo.mongodb.org/apt/ubuntu ' +
         '"$(lsb_release -sc)"/mongodb-org/3.0 multiverse" | sudo tee ' +
         '/etc/apt/sources.list.d/mongodb-org-3.0.list 2>&1',
-        'sudo apt-get update 2>&1'
-        'sudo apt-get install -y mongodb-org 2>&1'
+        'sudo apt-get update 2>&1',
+        'sudo apt-get install -y mongodb-org 2>&1',
         # enable access from any ip
         # by default access blocked to localhost
-        'sudo sed "s/bind_ip = /#bind_ip = /g" -i /etc/mongod.conf'
-        'sudo initctl stop mongod'
+        'sudo sed "s/bind_ip = /#bind_ip = /g" -i /etc/mongod.conf',
+        'sudo initctl stop mongod',
         'sudo initctl start mongod'
     ]
     _run("\n".join(script))
