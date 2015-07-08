cf430ef734512343f73b5138788ba7097cddeb1d
Denys Makogon <Denys@gigaspaces.com>
2015-07-08 13:49:43 +0300

    SCOR-77: Add local blueprint execution
     - targe: install_score_postgresql.py fabric script

--- blueprints/scripts/score/install_dependencies.py
+++ blueprints/scripts/score/install_dependencies.py
@@ -12,13 +12,21 @@ def _run(command):
 
 def install(config):
     ctx.logger.info("Config: " + str(config))
-    script = ["""
-sudo apt-get update
-sudo apt-get upgrade -y
-sudo apt-get install -qy build-essential libssl-dev libffi-dev libxml2-dev
-sudo apt-get install -qy libxslt-dev python-dev python-pip git libpq-dev
-sudo apt-get install -qy postgresql-client
-sudo apt-get install gunicorn -qy 2>&1
-sudo apt-get install nginx -qy 2>&1
-    """]
+    script = [
+        "sudo apt-get update",
+
+        'sudo apt-get upgrade -y '
+        '-o Dpkg::Options::="--force-confdef" '
+        '-o Dpkg::Options::="--force-confold"',
+
+        "sudo apt-get install -qy build-essential "
+        "libssl-dev libffi-dev libxml2-dev",
+
+        "sudo apt-get install -qy libxslt-dev python-dev "
+        "python-pip git libpq-dev",
+
+        "sudo apt-get install -qy postgresql-client",
+        "sudo apt-get install gunicorn -qy 2>&1",
+        "sudo apt-get install nginx -qy 2>&1"
+    ]
     _run("\n".join(script))
