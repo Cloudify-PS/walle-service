9577c554f08f657ad8b3aee28f30481044881cfa
Mykhailo Durnosvystov <Mikhail@gigaspaces.com>
2015-07-08 12:10:08 +0300

    Fixes creating db with credentials from inputs

--- blueprints/scripts/install_score_postgresql.py
+++ blueprints/scripts/install_score_postgresql.py
@@ -12,23 +12,34 @@ def _run(command):
 
 def install(config):
     ctx.logger.info("Config: " + str(config))
-    _run("""
-sudo -u postgres createuser -D -S -R  score
-sudo -u postgres psql -c \
-    "ALTER USER score WITH PASSWORD 'secret-password';"
-sudo -u postgres psql -c "CREATE DATABASE score;"
-sudo -u postgres psql -c "GRANT ALL ON DATABASE score TO score;"
+    script = []
+    db_user = config.get('db_user', None)
+    db_name = config.get('db_name', None)
+    db_pass = config.get('db_pass', None)
+    script.append('sudo -u postgres createuser -D -S -R  %s' % db_user)
+    script.append("""
+sudo -u postgres psql -c "ALTER USER %s WITH PASSWORD '%s';"
+    """ % (db_user, db_pass))
+    script.append("""
+sudo -u postgres psql -c "CREATE DATABASE %s;"
+    """ % db_name)
+    script.append("""
+sudo -u postgres psql -c "GRANT ALL ON DATABASE %s TO %s;"
+    """ % (db_name, db_user))
+    script.append("""
 sudo sed "s/#listen_addresses = 'localhost'/listen_addresses = '""" +
-         config.get('ip', "*") +
-         """'/g" \
-           -i /etc/postgresql/9.1/main/postgresql.conf
-         sudo cp /etc/postgresql/9.1/main/pg_hba.conf pg_hba.conf
-         sudo chmod 777 pg_hba.conf
-         echo "host score score """ +
-         config.get('ip', "127.0.0.1") +
-         """/24 md5" >> pg_hba.conf
-         sudo mv pg_hba.conf /etc/postgresql/9.1/main/pg_hba.conf
-         sudo chmod 640 /etc/postgresql/9.1/main/pg_hba.conf
-         sudo chown postgres:postgres /etc/postgresql/9.1/main/pg_hba.conf
-         sudo /etc/init.d/postgresql restart
-         """)
+                  config.get('ip', "*") +
+                  """'/g" -i /etc/postgresql/9.1/main/postgresql.conf
+                  sudo cp /etc/postgresql/9.1/main/pg_hba.conf pg_hba.conf
+                  sudo chmod 777 pg_hba.conf
+                  echo "host %s %s """ % (db_name, db_user) +
+                  config.get('ip', "127.0.0.1") +
+                  """/24 md5" >> pg_hba.conf
+    """)
+    script.append("""
+sudo mv pg_hba.conf /etc/postgresql/9.1/main/pg_hba.conf
+sudo chmod 640 /etc/postgresql/9.1/main/pg_hba.conf
+sudo chown postgres:postgres /etc/postgresql/9.1/main/pg_hba.conf
+sudo /etc/init.d/postgresql restart
+    """)
+    _run("\n".join(script))
