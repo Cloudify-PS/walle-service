9b129be0e1c369cac36e01bcc932b5c06d089e55
Denys Makogon <Denys@gigaspaces.com>
2015-07-12 08:03:51 -0700

    SCOR-82: Switch from python-bash to pure bash

--- blueprints/scripts/install_score_postgresql.py
+++ blueprints/scripts/install_score_postgresql.py
@@ -29,8 +29,8 @@ sudo -u postgres psql -c "GRANT ALL ON DATABASE %s TO %s;"
     script.append("""
 sudo sed "s/#listen_addresses = 'localhost'/listen_addresses = '""" +
                   config.get('ip', "*") +
-                  """'/g" -i /etc/postgresql/9.1/main/postgresql.conf
-                  sudo cp /etc/postgresql/9.1/main/pg_hba.conf pg_hba.conf
+                  """'/g" -i /etc/postgresql/9.3/main/postgresql.conf
+                  sudo cp /etc/postgresql/9.3/main/pg_hba.conf pg_hba.conf
                   sudo chmod 777 pg_hba.conf
                   echo "host %s %s """ % (db_name, db_user) +
                   config.get('ip_postgres', "127.0.0.1") +
@@ -40,9 +40,9 @@ sudo sed "s/#listen_addresses = 'localhost'/listen_addresses = '""" +
                   """/24 md5" >> pg_hba.conf
     """)
     script.append("""
-sudo mv pg_hba.conf /etc/postgresql/9.1/main/pg_hba.conf
-sudo chmod 640 /etc/postgresql/9.1/main/pg_hba.conf
-sudo chown postgres:postgres /etc/postgresql/9.1/main/pg_hba.conf
+sudo mv pg_hba.conf /etc/postgresql/9.3/main/pg_hba.conf
+sudo chmod 640 /etc/postgresql/9.3/main/pg_hba.conf
+sudo chown postgres:postgres /etc/postgresql/9.3/main/pg_hba.conf
 sudo /etc/init.d/postgresql restart
     """)
     _run("\n".join(script))
---
+++ blueprints/scripts/score/install_dependencies.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+sudo apt-get update
+
+sudo apt-get upgrade -y  -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" 2>&1
+
+sudo apt-get install -qy build-essential libssl-dev \
+ libffi-dev libxml2-dev postgresql-client \
+ gunicorn nginx libxslt-dev python-dev \
+ python-pip git libpq-dev 2>&1
