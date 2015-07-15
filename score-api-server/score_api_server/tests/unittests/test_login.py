cf040c59044ff26d18b199deec1c5eb04d8eff5b
Kostya <konstantin@gigaspaces.com>
2015-07-15 14:37:44 +0400

    SCOR-72 Style fix

--- score-api-server/score_api_server/tests/unittests/test_login.py
+++ score-api-server/score_api_server/tests/unittests/test_login.py
@@ -69,7 +69,7 @@ class TestBase(testtools.TestCase):
                                                       5, 6, None, None))
 
         self.assertTrue(login._login_user_to_service(1, 2, 3, 'subscription',
-                                                      5, 6, None, 8))
+                                                     5, 6, None, 8))
         fake_vca.login_to_org = mock.MagicMock(return_value=False)
         self.assertFalse(login._login_user_to_service(1, 2, 3, 'subscription',
                                                       5, 6, 7, 8))
