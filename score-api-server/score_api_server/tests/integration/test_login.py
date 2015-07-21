a1629fb0a19fc9bf0437b0a7958981cc84d47109
Kostya <konstantin@gigaspaces.com>
2015-07-21 10:18:18 +0400

    SCOR-72 Update integration test

--- score-api-server/score_api_server/tests/integration/test_login.py
+++ score-api-server/score_api_server/tests/integration/test_login.py
@@ -8,4 +8,5 @@ class TestApiLogin(base.IntegrationBaseTestCase):
     def test_spec(self):
         response = self.execute_post_request_with_route("/login")
         self.assertEqual(401, response.status_code)
-        self.assertEqual(response.data, 'Unauthorized.')
+        self.assertEqual(response.data, "Unauthorized. Empty request body"
+                         " or Content-Type not 'application/json'.")
