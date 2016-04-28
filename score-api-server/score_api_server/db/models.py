f8060fd09faad9ac1a25df806c62c84522301248
Denis Pauk <pauk.denis@gmail.com>
2016-04-28 11:57:40 +0300

    add new tables

--- score-api-server/score_api_server/db/models.py
+++ score-api-server/score_api_server/db/models.py
@@ -4,6 +4,68 @@ import yaml
 
 from score_api_server.db import base
 
+class AllowedKeyStoreUrl(base.BaseDatabaseModel, base.db.Model):
+    __tablename__ = 'allowed_ketstore_urls'
+
+    id = base.db.Column(base.db.String(), primary_key=True)
+    keystore_url = base.db.Column(base.db.String(), unique=True)
+    info = base.db.Column(base.db.String())
+    created_at = base.db.Column(base.db.String())
+
+    def __init__(self, keystore_url, info=None):
+        self.keystore_url = keystore_url
+        self.info = info if info else ""
+        super(AllowedKeyStoreUrl, self).__init__()
+        self.save()
+
+    def __repr__(self):
+        return '#{}: Allowed {}'.format(
+            self.id, self.keystore_url
+        )
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "keystore_url": self.keystore_url,
+            "info": self.info,
+            "created_at": self.created_at,
+        }
+
+class KeyStoreUrlToCloudifyAssociationWithLimits(base.BaseDatabaseModel,
+                                           base.db.Model):
+    __tablename__ = 'keystore_url_to_cloudify_with_limits'
+
+    id = base.db.Column(base.db.String(), primary_key=True)
+    keystore_url = base.db.Column(base.db.String(), unique=True)
+    deployment_limits = base.db.Column(base.db.Integer())
+    number_of_deployments = base.db.Column(base.db.Integer())
+    cloudify_host = base.db.Column(base.db.String())
+    cloudify_port = base.db.Column(base.db.String())
+    created_at = base.db.Column(base.db.String())
+    updated_at = base.db.Column(base.db.String())
+
+    def __init__(self, keystore_url, cloudify_host,
+                 cloudify_port, deployment_limits=0):
+        self.keystore_url = keystore_url
+        self.cloudify_host = cloudify_host
+        self.cloudify_port = cloudify_port
+        self.deployment_limits = deployment_limits
+        self.number_of_deployments = 0
+        super(KeyStoreUrlToCloudifyAssociationWithLimits, self).__init__()
+        self.save()
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "keystore_url": self.keystore_url,
+            "deployment_limits": self.deployment_limits,
+            "number_of_deployments": self.number_of_deployments,
+            "created_at": self.created_at,
+            "updated_at": self.updated_at,
+            "cloudify_host": self.cloudify_host,
+            "cloudify_port": self.cloudify_port
+        }
+
 
 class AllowedOrgs(base.BaseDatabaseModel, base.db.Model):
     __tablename__ = 'allowed_org_ids'
