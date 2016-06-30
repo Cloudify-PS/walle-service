4a73b0511e8d7d1d4a24d4febaee41c0457af828
Denis Pauk <pauk.denis@gmail.com>
2016-06-30 17:57:29 +0300

    install by pip

--- blueprints/scripts/walle/install_walle.sh
+++ blueprints/scripts/walle/install_walle.sh
@@ -13,6 +13,4 @@ curl -o walle-service.tar.gz ${WALLE_PACKAGE_URL}  2>&1
 mkdir -p walle-service
 tar -xvf walle-service.tar.gz -C walle-service 2>&1
 cd walle-service/
-git init
-cd walle-api-server/
-sudo python setup.py install 2>&1
+pip install ./walle-api-server
