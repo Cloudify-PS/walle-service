66e929bd472dcb10efbd53e762bb9a60aa4ba3ba
Denis Pauk <pauk.denis@gmail.com>
2016-07-08 17:16:51 +0300

    unittests fix

--- walle-api-server/walle_api_server/tests/unittests/test_walle_api_server_service_limit.py
+++ walle-api-server/walle_api_server/tests/unittests/test_walle_api_server_service_limit.py
@@ -6,26 +6,26 @@ import uuid
 from walle_api_server.common import service_limit
 from walle_api_server.cli import app
 from walle_api_server.db.models import Endpoint
-from walle_api_server.db.models import (
-    Tenant)
+from walle_api_server.db.models import Limit
+from walle_api_server.db.models import Tenant
 
 from walle_api_server.tests.unittests import base
 
 
-class CommonOrgLimitTest(base.BaseTestCaseWihtBackend):
+class EndpointTenantLimitTest(base.BaseTestCaseWihtBackend):
 
     def setUp(self):
-        super(CommonOrgLimitTest, self).setUp()
+        super(EndpointTenantLimitTest, self).setUp()
         self.prefix = str(uuid.uuid4())
-        self._create_limit_orgs()
+        self._create_tenant_orgs()
 
     def tearDown(self):
-        super(CommonOrgLimitTest, self).tearDown()
+        super(EndpointTenantLimitTest, self).tearDown()
 
-    def _create_limit_orgs(self):
+    def _create_tenant_orgs(self):
         # add some values
-        org = Endpoint('some_url', self.prefix + "some_id")
-        k_org = Endpoint('some_url', self.prefix + "k_id")
+        org = Endpoint(self.prefix + "some_id", "openstack")
+        k_org = Endpoint(self.prefix + "k_id", "openstack")
         self.obj_list.append(k_org)
         self.obj_list.append(org)
 
@@ -34,80 +34,83 @@ class CommonOrgLimitTest(base.BaseTestCaseWihtBackend):
 
         with app.app.app_context():
             # check value for current existed
-            flask.g.tenant_id = self.prefix + "some_id"
-            limit = service_limit.check_endpoint_url('some_url',
-                                                     flask.g.tenant_id)
-            self.assertTrue(limit)
+            flask.g.endpoint_url = self.prefix + "some_id"
+            endpoint = service_limit.check_endpoint_url(
+                flask.g.endpoint_url, "openstack"
+            )
+            self.assertTrue(endpoint)
             self.assertIn(self.prefix + "some_id",
-                          limit.tenant)
+                          endpoint.endpoint)
             # check count
-            flask.g.tenant_id = self.prefix + "k_id"
-            limit = service_limit.check_endpoint_url('some_url',
-                                                     flask.g.tenant_id)
-            self.assertTrue(limit)
-            self.assertIn(self.prefix + "k_id", limit.tenant)
+            flask.g.endpoint_url = self.prefix + "k_id"
+            endpoint = service_limit.check_endpoint_url(
+                flask.g.endpoint_url, "openstack"
+            )
+            self.assertTrue(endpoint)
+            self.assertIn(self.prefix + "k_id", endpoint.endpoint)
             # no orgs
-            flask.g.tenant_id = self.prefix + "some_other_id"
-            limit = service_limit.check_endpoint_url('some_url',
-                                                     flask.g.tenant_id)
-            self.assertFalse(limit)
+            flask.g.endpoint_url = self.prefix + "some_other_id"
+            endpoint = service_limit.check_endpoint_url(
+                flask.g.endpoint_url, "openstack"
+            )
+            self.assertFalse(endpoint)
 
 
 class TestDeploymentLimitsDBModel(base.BaseTestCaseWihtBackend):
 
     def setUp(self):
         super(TestDeploymentLimitsDBModel, self).setUp()
-        self.allowed_service_url = Endpoint(
-            'some_url',
-            str(uuid.uuid4()),
-            info="test_org_id"
+        self.allowed_endpoint = Endpoint(
+            str(uuid.uuid4()), type='openstack', version='*',
+            description="test_org_id"
         )
-        self.obj_list.append(self.allowed_service_url)
+        self.obj_list.append(self.allowed_endpoint)
 
     def tearDown(self):
         super(TestDeploymentLimitsDBModel, self).tearDown()
 
-    def create_limit(self):
+    def create_tenant(self):
         return Tenant(
-            self.allowed_service_url.id,
+            self.allowed_endpoint.id,
+            "some_tenant",
             "127.0.0.1",
-            "80",
+            "80"
         )
 
     def test_create_successfuly(self):
-        successful_limit = self.create_limit()
+        successful_tenant = self.create_tenant()
 
-        self.assertIsNotNone(successful_limit)
-        self.assertIsNotNone(successful_limit.endpoint_id)
-        self.assertEqual(self.allowed_service_url.id,
-                         successful_limit.endpoint_id)
-        self.assertIsNotNone(successful_limit.id)
-        self.assertIsNotNone(successful_limit.created_at)
-        self.assertIsNotNone(successful_limit.updated_at)
+        self.assertIsNotNone(successful_tenant)
+        self.assertIsNotNone(successful_tenant.endpoint_id)
+        self.assertEqual(self.allowed_endpoint.id,
+                         successful_tenant.endpoint_id)
+        self.assertIsNotNone(successful_tenant.id)
+        self.assertIsNotNone(successful_tenant.created_at)
+        self.assertIsNotNone(successful_tenant.updated_at)
         self.assertEqual("127.0.0.1",
-                         successful_limit.cloudify_host)
-        self.assertEqual("80", successful_limit.cloudify_port)
+                         successful_tenant.cloudify_host)
+        self.assertEqual(80, successful_tenant.cloudify_port)
 
-        successful_limit.delete()
+        successful_tenant.delete()
 
     def test_create_duplicate_org_id_host_port(self):
-        limit = self.create_limit()
+        limit = self.create_tenant()
 
         self.assertRaises(Exception,
                           Tenant.__init__,
-                          self.allowed_service_url.id,
+                          self.allowed_endpoint.id,
                           "127.0.0.1",
                           "80")
         self.assertRaises(Exception,
                           Tenant.__init__,
-                          self.allowed_service_url.id,
+                          self.allowed_endpoint.id,
                           "127.0.0.2",
                           "8889")
 
         limit.delete()
 
     def test_list_limits(self):
-        limit = self.create_limit()
+        limit = self.create_tenant()
 
         limits = Tenant.list()
         self.assertNotEqual(0, len(limits))
@@ -115,7 +118,7 @@ class TestDeploymentLimitsDBModel(base.BaseTestCaseWihtBackend):
         limit.delete()
 
     def test_find_by(self):
-        limit = self.create_limit()
+        limit = self.create_tenant()
 
         limit_by_id = Tenant.find_by(
             id=limit.id)
@@ -126,7 +129,7 @@ class TestDeploymentLimitsDBModel(base.BaseTestCaseWihtBackend):
         limit.delete()
 
     def test_update(self):
-        limit = self.create_limit()
+        limit = self.create_tenant()
 
         patched_limit = limit.update(cloudify_host="127.0.0.100",
                                      deployment_limits=100,
@@ -137,15 +140,30 @@ class TestDeploymentLimitsDBModel(base.BaseTestCaseWihtBackend):
         patched_limit.delete()
 
     def test_get_current_credentials_and_limits(self):
-        limit = self.create_limit()
+        tenant = self.create_tenant()
 
         with app.app.app_context():
-            flask.g.tenant_id = self.allowed_service_url.tenant
-            _limit = (
-                service_limit.get_endpoint_tenant('some_url',
-                                                  flask.g.tenant_id))
-            self.assertEqual(limit.cloudify_host,
-                             _limit.cloudify_host)
-            self.assertEqual(limit.cloudify_port,
-                             _limit.cloudify_port)
-            self.assertEqual(limit.endpoint_id, _limit.endpoint_id)
+            flask.g.endpoint_url = self.allowed_endpoint.endpoint
+            _tenant = service_limit.get_endpoint_tenant(
+                flask.g.endpoint_url, "openstack", "some_tenant"
+            )
+            self.assertEqual(tenant.cloudify_host,
+                             _tenant.cloudify_host)
+            self.assertEqual(tenant.cloudify_port,
+                             _tenant.cloudify_port)
+            self.assertEqual(tenant.endpoint_id, _tenant.endpoint_id)
+
+            limit = Limit(
+                tenant.id, soft=100, hard=50, type="ram", value=-5
+            )
+
+            _limit = service_limit.get_endpoint_tenant_limit(
+                flask.g.endpoint_url, "openstack", "some_tenant", "none"
+            )
+            self.assertFalse(_limit)
+            _limit = service_limit.get_endpoint_tenant_limit(
+                flask.g.endpoint_url, "openstack", "some_tenant", "ram"
+            )
+            self.assertEqual(limit.hard, _limit.hard)
+            self.assertEqual(limit.soft, _limit.soft)
+            self.assertEqual(limit.value, _limit.value)
