dec10414b311d8992187be227cc96e2a93cf7cd8
Denis Pauk <pauk.denis@gmail.com>
2016-08-03 18:25:43 +0300

    split manage to tenantlimit and plugins

--- walle-api-server/walle_api_server/resources/resources.py
+++ walle-api-server/walle_api_server/resources/resources.py
@@ -11,7 +11,9 @@ from walle_api_server.resources import login_vcloud
 from walle_api_server.resources import login_openstack
 from walle_api_server.resources import login_walle
 from walle_api_server.resources import endpoints
-from walle_api_server.resources import manage
+from walle_api_server.resources import plugins
+from walle_api_server.resources import tenants
+from walle_api_server.resources import tenantlimits
 from walle_api_server.resources import service
 from walle_api_server.resources import nodes
 
@@ -43,17 +45,17 @@ def setup_resources(api):
     # look to rights table, partial copy of manage section
     api.add_resource(endpoints.Endpoints, '/endpoints')
     api.add_resource(endpoints.EndpointsId, '/endpoints/<string:id>/')
-    api.add_resource(manage.Tenants, '/tenants')
-    api.add_resource(manage.TenantsId, '/tenants/<string:id>')
-    api.add_resource(manage.Limits, '/limits')
-    api.add_resource(manage.LimitsId, '/limits/<string:id>')
+    api.add_resource(tenants.Tenants, '/tenants')
+    api.add_resource(tenants.TenantsId, '/tenants/<string:id>')
+    api.add_resource(tenantlimits.Limits, '/limits')
+    api.add_resource(tenantlimits.LimitsId, '/limits/<string:id>')
 
     # walle admin part, will check walle auth
     api.add_resource(login_walle.LoginWalle, '/login_walle')
-    api.add_resource(manage.ApprovedPlugins, '/manage/approved_plugins')
-    api.add_resource(manage.ApprovedPluginsFromFile,
+    api.add_resource(plugins.ApprovedPlugins, '/manage/approved_plugins')
+    api.add_resource(plugins.ApprovedPluginsFromFile,
                      '/manage/approved_plugins/from_file')
-    api.add_resource(manage.ApprovedPluginsId,
+    api.add_resource(plugins.ApprovedPluginsId,
                      '/manage/approved_plugins/<string:name>')
     api.add_resource(service.Maintenance, '/maintenance')
     api.add_resource(nodes.Nodes, '/nodes')
